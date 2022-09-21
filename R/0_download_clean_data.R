f085723bcd2432f60e933bdbdd4e83134fa76dec
unknown <baltazargch@gmail.com>
2022-09-21 12:43:52 -0300

    Initial coding

---
+++ R/0_download_clean_data.R
@@ -0,0 +1,13 @@
+###############################################################
+## DATA GATHERING AND CLEANING: OCCURRENCE POINTS AND RANGES ##
+###############################################################
+
+#### Load libraries ####
+library(rgbif)
+library(CoordinateCleaner)
+library(redlistr)
+library(sf)
+
+#### General parameters ####
+sf_use_s2(FALSE)
+
