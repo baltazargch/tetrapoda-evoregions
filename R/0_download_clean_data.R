2288bef1b25ecb01e8a340edcb2cae09d0ed7605
unknown <baltazargch@gmail.com>
2022-09-21 13:50:38 -0300

    with download rgbig

--- R/0_download_clean_data.R
+++ R/0_download_clean_data.R
@@ -10,4 +10,23 @@ library(sf)
 
 #### General parameters ####
 sf_use_s2(FALSE)
+# usethis::edit_r_environ("project")
 
+#### Download taxonomy from GBIF and IUCN ####
+taxonKeys <- c(
+  # "Amphibia" = 131,
+  # "Aves" = 212,
+  # "Reptilia" = 358,
+  "Mammalia" = 359
+)
+taxGBIF <- occ_download(
+  pred_and(
+    pred_in("taxonKey", taxonKeys),
+    pred("hasCoordinate", TRUE)
+  ),
+  format = 'SIMPLE_CSV'
+)
+
+if(!dir.exists('inputs/gbif')) dir.create('inputs/gbif', recursive = TRUE)
+
+occ_download_get(taxGBIF, path = 'inputs/gbif/')
