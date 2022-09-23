551d9d6744a0a81aeb5b70dae44f3f8fcca91747
unknown <baltazargch@gmail.com>
2022-09-23 20:18:48 -0300

    asd

--- R/0_download_clean_data.R
+++ R/0_download_clean_data.R
@@ -7,6 +7,7 @@ library(rgbif)
 library(CoordinateCleaner)
 library(redlistr)
 library(sf)
+library(tidyverse)
 
 #### General parameters ####
 sf_use_s2(FALSE)
@@ -17,7 +18,8 @@ taxonKeys <- c(
   # "Amphibia" = 131,
   # "Aves" = 212,
   # "Reptilia" = 358,
-  "Mammalia" = 359
+  # "Mammalia" = 359
+  732, 792 #"Carnivora"
 )
 taxGBIF <- occ_download(
   pred_and(
@@ -29,4 +31,19 @@ taxGBIF <- occ_download(
 
 if(!dir.exists('inputs/gbif')) dir.create('inputs/gbif', recursive = TRUE)
 
-occ_download_get(taxGBIF, path = 'inputs/gbif/')
+status = occ_download_meta(taxGBIF)$status
+count = 1
+while(status != 'SUCCEEDED' | count > 20){
+  Sys.sleep(120)
+  status = occ_download_meta(taxGBIF)$status
+  count = count + 1
+}
+
+x <- occ_download_get(taxGBIF, path = 'inputs/gbif', overwrite =FALSE)
+
+x <- as.download("inputs/gbif/0021573-220831081235567.zip")
+occs <- occ_download_import(x)
+colnames(occs) <- tolower(colnames(occs))
+
+
+occs_clean <- CoordinateCleaner::clean_coordinates(occs)
